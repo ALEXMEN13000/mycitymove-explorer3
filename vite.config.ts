fa0c7d10ac5f1ebfaebfe7018945ae0f75fb9e47
Alexis Meniante <alexismeniante@MacBook-Air-de-Alexis.local>
2025-01-16 14:28:43 +0100

    Fix base URL configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
   resolve: {
     alias: {
@@ -14,5 +14,5 @@ export default defineConfig({
     port: 3000,
     open: true
   },
-  base: '/mycitymove-explorer-web/'
-});
+  base: command === 'serve' ? '/' : '/mycitymove-explorer-web/'
+}));
